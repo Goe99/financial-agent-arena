69a58e8d07797067c1c5e60cbf0d582a4f672e0c
Joe <86591733+Goe99@users.noreply.github.com>
2026-09-16 12:00:45 +0800

    style: refine audit white workspace details

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.js";
 import "./styles.css";
+import "./polish.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
